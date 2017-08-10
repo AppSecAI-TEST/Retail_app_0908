d9554c240a4bf6ea2936fa756457901ae6069030
Avishek Mishra <avishekm14@gmail.com>
2017-08-10 17:05:32 +0530

    Changes on 9-8-2017

--- app/src/main/java/com/example/fnspl/retail_app/Actvities/fragment/Mycart.java
+++ app/src/main/java/com/example/fnspl/retail_app/Actvities/fragment/Mycart.java
@@ -1,14 +1,18 @@
 package com.example.fnspl.retail_app.Actvities.fragment;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -56,6 +60,7 @@ public class Mycart extends DialogFragment {
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
         binding.recyclerGrocery.setLayoutManager(layoutManager);
+        binding.recyclerGrocery.addItemDecoration(new SimpleDividerItemDecoration(getActivity()));
 
         adapter = new MyCartAdapter(getActivity(),data);
         binding.recyclerGrocery.setAdapter(adapter);
@@ -91,5 +96,32 @@ public class Mycart extends DialogFragment {
 
     public void closeDialog(){
         dismiss();
+    }
+
+    public class SimpleDividerItemDecoration extends RecyclerView.ItemDecoration {
+        private Drawable mDivider;
+
+        public SimpleDividerItemDecoration(Context context) {
+            mDivider = context.getResources().getDrawable(R.drawable.line_divider);
+        }
+
+        @Override
+        public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+            int left = parent.getPaddingLeft();
+            int right = parent.getWidth() - parent.getPaddingRight();
+
+            int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View child = parent.getChildAt(i);
+
+                RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+                int top = child.getBottom() + params.bottomMargin;
+                int bottom = top + mDivider.getIntrinsicHeight();
+
+                mDivider.setBounds(left, top, right, bottom);
+                mDivider.draw(c);
+            }
+        }
     }
 }
